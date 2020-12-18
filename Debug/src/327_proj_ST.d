0e839bff5a6dcfe716e0581267566600984ddccb
andrew8077 <71798287+andrew8077@users.noreply.github.com>
2020-12-17 21:49:24 -0500

    initial commit

---
+++ Debug/src/327_proj_ST.d
@@ -0,0 +1,25 @@
+src/327_proj_ST.o: ../src/327_proj_ST.cpp \
+ ../src/../../327_proj_ST_lib/includes/Smalltalk_American.h \
+ ../src/../../327_proj_ST_lib/includes/Smalltalk.h \
+ ../src/../../327_proj_ST_lib/includes/Watch.h \
+ ../src/../../327_proj_ST_lib/includes/ST_American_DonutEnthusiest.h \
+ ../src/../../327_proj_ST_lib/includes/Smalltalk_Brit.h \
+ ../src/../../327_proj_ST_lib/includes/Watch.h \
+ ../src/../../327_proj_ST_lib/includes/constants.h \
+ ../src/../../327_proj_ST_lib/includes/Functions.h
+
+../src/../../327_proj_ST_lib/includes/Smalltalk_American.h:
+
+../src/../../327_proj_ST_lib/includes/Smalltalk.h:
+
+../src/../../327_proj_ST_lib/includes/Watch.h:
+
+../src/../../327_proj_ST_lib/includes/ST_American_DonutEnthusiest.h:
+
+../src/../../327_proj_ST_lib/includes/Smalltalk_Brit.h:
+
+../src/../../327_proj_ST_lib/includes/Watch.h:
+
+../src/../../327_proj_ST_lib/includes/constants.h:
+
+../src/../../327_proj_ST_lib/includes/Functions.h:
